bedec85a4f04bf1ac142a5ac31b4ba6aded4a13d
Tanya <tanyasharma7658@gmail.com>
2025-06-06 14:04:41 +0530

    Update Div3B.cpp

--- CodeChef Solutions/CC Starters 189 /Div3B.cpp
+++ CodeChef Solutions/CC Starters 189 /Div3B.cpp
@@ -3,38 +3,40 @@
 // , where X represents the number of distinct vitamins eaten, and Y represents the total cost of all the food items. 
 // Find this maximum possible value. It is also possible to buy no food item at all, in which case the answer is simply 0.
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int t;
-	cin>>t;
-	while(t--){
-	    int n,c;
-	    cin>>n>>c;
-	    vector<int> a(n);
-	    for(int i=0;i<n;i++) cin>>a[i];
-	    vector<int> b(n);
-	    for(int i=0;i<n;i++) cin>>b[i];
-	    
-	    unordered_map<int,int> mp; // vitamin:cost 
-	    for(int i=0;i<n;i++){
-	        int vitamin=a[i];
-	        int cost=b[i];
-	        if(!mp.count(vitamin)||cost<mp[vitamin]) mp[vitamin]=cost;
-	    }
-	    
-	    vector<int> cost;
-	    for(auto i:mp) cost.push_back(i.second);
-	    
-	    sort(cost.begin(),cost.end());
-	    int y=0,x=0,maxi=0;
-	    for(int i=0;i<cost.size();i++){
-	        y+=cost[i];
-	        x=i+1;
-	        maxi=max(maxi,c*x-y);
-	    }
-	    cout<<maxi<<endl;
-	}
-	return 0;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,c;
+        cin>>n>>c;
+        vector<int> a(n),b(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        for(int i=0;i<n;i++) cin>>b[i];
+        
+        unordered_map<int,int> mp; //vit:cost 
+        for(int i=0;i<n;i++){
+            int vit=a[i];
+            int cost=b[i];
+            
+            if(mp.find(vit)==mp.end() || cost<mp[vit]) mp[vit]=cost;
+        }
+       
+        vector<int> costs;
+        for(auto i:mp) costs.push_back(i.second);
+       
+        sort(costs.begin(),costs.end());
+        
+        int x=0,y=0,ans=0;
+        for(int i=0;i<costs.size();i++){
+            x=i+1;
+            y+=costs[i];
+            ans=max(ans,c*x-y);
+        }
+        cout<<ans<<endl;
+    }
+    
+    return 0;
 }
